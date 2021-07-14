b992b059c03b2df7269cbb4e506252681f182653
anusha <anushachokka@gmail.com>
2021-07-13 18:32:44 -0700

    Netflix-clone first commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+function HomeScreen() {
+  return (
+    <div className="HomeScreen">
+      {/*nav bar*/}
+      <Nav />
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB29F6WC2-J8KNLfXk9EU65KObUqT_lNCc",
+  authDomain: "netflix-clone-85b60.firebaseapp.com",
+  projectId: "netflix-clone-85b60",
+  storageBucket: "netflix-clone-85b60.appspot.com",
+  messagingSenderId: "359739699501",
+  appId: "1:359739699501:web:d2b6ad886b2cda7820e3a4",
+};
